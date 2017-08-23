c7b50b47924ed184fbada40eb32835d0809df298
Fedde Murua <fedde.murua@gmail.com>
2017-08-22 21:14:17 -0300

    primer login a modificar

--- desarrollo/desarrollo/settings.py
+++ desarrollo/desarrollo/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #aplicaciones
     'liquidacion',
     'persona',
+    
+    #librerias
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'desarrollo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "estaticos"),
+)
+
+LOGIN_REDIRECT_URL = "home"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'estaticos/images/upload')
