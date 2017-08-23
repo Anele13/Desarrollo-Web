0e486930426c7939d6977c442dfc19fd091d34a8
anele <anele_13@hotmail.com>
2017-08-23 10:02:44 -0300

    ajustes persona usuario

--- desarrollo/desarrollo/settings.py
+++ desarrollo/desarrollo/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     #aplicaciones
     'liquidacion',
     'persona',
-    
+
     #librerias
     'bootstrap3',
 ]
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL='persona.Usuario'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "estaticos"),
--- desarrollo/persona/models.py
+++ desarrollo/persona/models.py
@@ -1,7 +1,35 @@
+from __future__ import unicode_literals
 from django.db import models
 from .choices import *
+from django.contrib.auth.models import AbstractUser, Group
 
-class Agente (models.Model):
+class Rol(models.Model):
+    class Meta:
+        abstract= True
+
+
+class Agente(Rol):
+     def get_view_name(self):
+        return "Agente"
+
+
+class Administrador(Rol):
+     def get_view_name(self):
+        return "Administrador"
+
+
+class Usuario(Rol, AbstractUser):
+    AGENTE = "AGENTE"
+    ADMINISTRADOR = "ADMINISTRADOR"
+    
+    def get_view_name(self):
+        return self.groups.first().name
+
+    def get_view_groups(self):
+        return self.groups.all()
+
+
+class Persona(models.Model):
     documento  = models.BigIntegerField()
     tipo_doc = models.CharField(max_length=10, choices=Tipo)
     #nya #NOMBRE Y APELLIDO, O NYA?
@@ -14,7 +42,10 @@ class Agente (models.Model):
     observ = models.CharField(max_length=255)
     direccion = models.ForeignKey('Direccion', on_delete=models.CASCADE)
     revisado = models.CharField(max_length=10)
+    usuario = models.OneToOneField(Usuario, blank=True, null=True)
 
+    def crear_usuario(self, nombre, clave):
+        self.usuario = Usuario.objects.create_user(username=nombre, password=clave)
 
 class Direccion(models.Model):
     provincia = models.IntegerField()
