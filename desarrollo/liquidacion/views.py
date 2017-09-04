15d4bb966f1c278fe4f2655da5e8b33c37cd16ed
anele <anele_13@hotmail.com>
2017-09-04 11:31:26 -0300

    agregado de filtros y vista agente

--- desarrollo/liquidacion/views.py
+++ desarrollo/liquidacion/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
 from django.utils.html import format_html
 from .models import *
-
 import pandas as pd
 
-def nada():
-    pass
+def liquidaciones(request):
+    return render(request, 'persona/prueba.html')
 
-def prueba(request):
-
-    df = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=11001005).values()),columns=["concepto_id","mes_id","monto"])
-
-    #qs = pd.crosstab(df["concepto_id"], df["mes_id"],margins=True)
-    #qs=pd.pivot_table(df,index=["concepto_id", "monto"], columns=["mes_id"], fill_value=0)
-
+def extra(request, mes=None):
+    df = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=11001005, mes=mes).values()),columns=["concepto_id","mes_id","monto"])
     qs=pd.pivot_table(df,index=["concepto_id"], columns=["mes_id"], values="monto", fill_value=0)
     resul = format_html(qs.to_html())
     return render(request, 'persona/prueba.html', {'resul':resul})
