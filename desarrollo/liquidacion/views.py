9ebaac059b1c667f383a125c9bd16de4d62a7353
Fedde <fedde.murua@gmail.com>
2017-09-04 10:28:19 -0300

    join para mostrar ordenliq

--- desarrollo/liquidacion/views.py
+++ desarrollo/liquidacion/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.utils.html import format_html
 from .models import *
-
 import pandas as pd
 
 def nada():
@@ -9,11 +8,16 @@ def nada():
 
 def prueba(request):
 
-    df = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=11001005).values()),columns=["concepto_id","mes_id","monto"])
+    #df_mes = pd.DataFrame(list(Mes.objects.all().values()),columns=["id","nombre"])
+    df_liquidacion_concepto = pd.DataFrame(list(Concepto.objects.all().filter(ordenliq__isnull=False).values()),columns=["concepto","descrip"])
+    df_hliquidac = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=11001005).values()),columns=["concepto_id","mes_id","monto"])
+    df_join = df_hliquidac.set_index('concepto_id').join(df_liquidacion_concepto.set_index('concepto')) # Muestra ordenliq =/ NULL
+    #df_resul = df_join.set_index('mes_id').join(df_mes.set_index('id')) # Join con tabla meses para mostrar nombre
 
-    #qs = pd.crosstab(df["concepto_id"], df["mes_id"],margins=True)
-    #qs=pd.pivot_table(df,index=["concepto_id", "monto"], columns=["mes_id"], fill_value=0)
+    print(df_join)
 
-    qs=pd.pivot_table(df,index=["concepto_id"], columns=["mes_id"], values="monto", fill_value=0)
+    qs=pd.pivot_table(df_join,index=["descrip"], columns=["mes_id"], values="monto", fill_value=0)
+
+
     resul = format_html(qs.to_html())
     return render(request, 'persona/prueba.html', {'resul':resul})
