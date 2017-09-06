7d905e5495455d7dec898431f3d786a7d541da76
anele <anele_13@hotmail.com>
2017-09-06 11:30:00 -0300

    agregado saco el documento del usuario de la sesion en admin liquidacion

--- desarrollo/liquidacion/views.py
+++ desarrollo/liquidacion/views.py
@@ -6,11 +6,11 @@ from easy_pdf.views import PDFTemplateView
 from easy_pdf.rendering import render_to_pdf
 
 def liquidaciones(request, mes=None):
-
+    usuario=request.user
     if mes:
         df_mes = pd.DataFrame(list(Mes.objects.all().values()),columns=["id","nombre"])
         df_liquidacion_concepto = pd.DataFrame(list(Concepto.objects.all().filter(ordenliq__isnull=False).values()),columns=["concepto","descrip"])
-        df_hliquidac = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=11001005,mes=mes).values()),columns=["concepto_id","mes_id","monto"])
+        df_hliquidac = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=usuario.persona.documento,mes=mes).values()),columns=["concepto_id","mes_id","monto"])
         liquidacion_concepto= df_hliquidac.set_index('concepto_id').join(df_liquidacion_concepto.set_index('concepto')) # Muestra ordenliq =/ NULL
         liquidacion_concepto_mes = liquidacion_concepto.set_index('mes_id').join(df_mes.set_index('id')) # Join con tabla meses para mostrar nombre
         qs=pd.pivot_table(liquidacion_concepto_mes,index=["descrip"], columns=["nombre"], values="monto", fill_value=0)
@@ -18,7 +18,7 @@ def liquidaciones(request, mes=None):
         #LISTA COMPLETA LIQUIDACIONES DEL AGENTE
         df_mes2 = pd.DataFrame(list(Mes.objects.all().values()),columns=["id","nombre"])
         df_liquidacion_concepto2 = pd.DataFrame(list(Concepto.objects.all().filter(ordenliq__isnull=False).values()),columns=["concepto","descrip"])
-        df_hliquidac2 = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=11001005).values()),columns=["concepto_id","mes_id","monto"])
+        df_hliquidac2 = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=usuario.persona.documento).values()),columns=["concepto_id","mes_id","monto"])
         liquidacion_concepto2= df_hliquidac2.set_index('concepto_id').join(df_liquidacion_concepto2.set_index('concepto')) # Muestra ordenliq =/ NULL
         liquidacion_concepto_mes2 = liquidacion_concepto2.set_index('mes_id').join(df_mes2.set_index('id')) # Join con tabla meses para mostrar nombre
         qs2=pd.pivot_table(liquidacion_concepto_mes2,index=["descrip"], columns=["nombre"], values="monto", fill_value=0)
@@ -28,7 +28,7 @@ def liquidaciones(request, mes=None):
     else:
         df_mes = pd.DataFrame(list(Mes.objects.all().values()),columns=["id","nombre"])
         df_liquidacion_concepto = pd.DataFrame(list(Concepto.objects.all().filter(ordenliq__isnull=False).values()),columns=["concepto","descrip"])
-        df_hliquidac = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=11001005).values()),columns=["concepto_id","mes_id","monto"])
+        df_hliquidac = pd.DataFrame(list(Hliquidac.objects.all().filter(documento=usuario.persona.documento).values()),columns=["concepto_id","mes_id","monto"])
         liquidacion_concepto= df_hliquidac.set_index('concepto_id').join(df_liquidacion_concepto.set_index('concepto')) # Muestra ordenliq =/ NULL
         liquidacion_concepto_mes = liquidacion_concepto.set_index('mes_id').join(df_mes.set_index('id')) # Join con tabla meses para mostrar nombre
         qs=pd.pivot_table(liquidacion_concepto_mes,index=["descrip"], columns=["nombre"], values="monto", fill_value=0)
