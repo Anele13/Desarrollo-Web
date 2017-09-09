9328ad4a1c61514c6a53f9ebceef9fc397e93378
anele <anee@gmail.com>
2017-09-09 19:14:51 -0300

    nada

--- desarrollo/desarrollo/settings.py
+++ desarrollo/desarrollo/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_economia',
         'USER': 'postgres',
-        'PASSWORD': 'holamundo',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
