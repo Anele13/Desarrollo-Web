199e29c26193fda17a9295f2777a35279501011b
anele <anele_13@hotmail.com>
2017-09-20 11:18:27 -0300

    borrado de path y objeto documento luego de cargar archivo csv

--- desarrollo/documento/views.py
+++ desarrollo/documento/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render, redirect
 from django.shortcuts import render
 from documento.forms import UploadForm
 from documento.models import Documento
+from django.shortcuts import render, redirect
+import os
 
-
+# Create your views here.
 def subir_archivo(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
             newdoc = Documento(filename = request.POST['filename'],docfile = request.FILES['docfile'])
             newdoc.save()
-            newdoc.csv_to_base(newdoc)                        
+            newdoc.csv_to_base(newdoc)
+            newdoc.delete()
+            if os.path.isfile(newdoc.docfile.path):
+                os.remove(newdoc.docfile.path)
             return redirect("uploads")
     else:
         form = UploadForm()
