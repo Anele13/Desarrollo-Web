3593f4d67b3a7db4b358e8801ab9db0a28415ff9
Fedde <fedde.murua@gmail.com>
2017-09-22 12:02:22 -0300

    modificacion de archivo de archivos con comillas

--- desarrollo/documento/models.py
+++ desarrollo/documento/models.py
@@ -6,7 +6,24 @@ class Documento(models.Model):
  filename = models.CharField(max_length=100)
  docfile = models.FileField(upload_to='csv/')
 
+ def reemplazar_comillas(self,doc):
+     char1 = chr(34)
+     char2 = ' '
+
+     filer = open(doc.docfile.path, "r")
+     filew = open(doc.docfile.path+'.txt', "w")
+
+     buff = filer.read()
+     rbuff = buff.replace(char1, char2)
+
+     filew.write(rbuff)
+
+     filer.close()
+     filew.close()
+
+
  def csv_to_base(self,documento):
+     self.reemplazar_comillas(documento)
      engine = create_engine('postgresql://postgres:holamundo@localhost:5432/db_economia')
      archivocsv=pandas.DataFrame.from_csv(documento.docfile.path, sep=',')
      engine.execute("DELETE FROM "+documento.filename)
