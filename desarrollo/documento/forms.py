d829092166b0fb9ac0103aeccf9b6425433df521
anele <anele_13@hotmail.com>
2017-09-25 08:45:37 -0300

    arreglos

--- desarrollo/documento/forms.py
+++ desarrollo/documento/forms.py
@@ -3,24 +3,24 @@ from django import forms
 import csv, operator
 from sqlalchemy import create_engine
 
-def crear():
+def listar_tablas():
     engine = create_engine('postgresql://postgres:holamundo@localhost:5432/db_economia', pool_recycle=3600)
-    lista_a_borrar=['django_session',
-                    'django_migrations',
-                    'auth_group',
-                    'auth_group_permissions',
-                    'auth_permission',
-                    'django_content_type',
-                    'usuario',
-                    'django_permission',
-                    'django_admin',
-                    'persona_usuario_groups',
-                    'persona_usuario_user_permissions',
-                    'django_admin_log',
-                    'documento']
+    elementos_a_borrar=['django_session',
+                        'django_migrations',
+                        'auth_group',
+                        'auth_group_permissions',
+                        'auth_permission',
+                        'django_content_type',
+                        'usuario',
+                        'django_permission',
+                        'django_admin',
+                        'persona_usuario_groups',
+                        'persona_usuario_user_permissions',
+                        'django_admin_log',
+                        'documento']
     diccionario={}
-    for tabla in engine.table_names():
-        if tabla not in lista_a_borrar:
+    for nombre_tabla in engine.table_names():
+        if nombre_tabla not in elementos_a_borrar:
             diccionario[tabla]=tabla
     return (tuple(diccionario.items()))
 
@@ -30,4 +30,4 @@ class UploadForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(UploadForm, self).__init__(*args, **kwargs)
-        self.fields['tabla'] = forms.ChoiceField(choices=crear())
+        self.fields['tabla'] = forms.ChoiceField(choices=listar_tablas())
